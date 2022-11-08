95a26b94ec3d77beb17fdb6e903f4701fb15c53d
sivaroj limvipuwat <sivarojlimvipuwat@sivarojs-MacBook-Pro.local>
2022-11-07 16:53:20 -0800

    add default values to node pools

--- gcp/modules/node_pools/variables.tf
+++ gcp/modules/node_pools/variables.tf
@@ -4,12 +4,20 @@ variable "cluster_name" {
 
 variable "cluster_node_pool_count" {
   type = number
+  default = 2
 }
 
 variable "cluster_node_type" {
   type = string
+  default = "e2-medium"
 }
 
-variable "cluster_node_disk_size" {}
+variable "cluster_node_disk_size" {
+  type = number
+  default = 10
+}
 
-variable "region" {}
+variable "region" {
+  type = string
+  default = "us-central1"
+}

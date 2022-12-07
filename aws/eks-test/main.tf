24bd0f57c0b09fc8fb6f84e292bd12948af09fb3
sivaroj limvipuwat <sivarojlimvipuwat@sivarojs-MacBook-Pro.local>
2022-12-06 21:16:20 -0800

    add examples for putting postgres env var

--- aws/eks-test/main.tf
+++ aws/eks-test/main.tf
@@ -2,6 +2,7 @@
 module "eks" {
   source       = "../modules/eks"
   cluster_name = "test123"
+  helm_release_name  = "test"
   helm_path    = "../modules/eks/helm/testchart"
   values = [{
     name  = "repoUri",
@@ -25,9 +26,8 @@ module "eks" {
     }
   ]
   AWS_REGION         = "us-east-1"
-  helm_release_name  = "test"
   max_node_count     = 6
   desired_node_count = 4
   min_node_count     = 4
-  env_variables      = { "env" : { "test1" : "test", "test2" : "test" } }
+  env_variables      = { "env" : { "DB_HOST" : "postgresql://example:example@postgres.com:1234", "test2" : "test" } }
 }
